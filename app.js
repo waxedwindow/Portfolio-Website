84f9865a2c026c12f29c7c6ccc499e0727082e57
waxedwindow <nayeemsultan2006@gmail.com>
2024-09-28 16:42:50 +0100

    inital upload

---
+++ app.js
@@ -0,0 +1,18 @@
+
+let baesection = document.getElementById("BAEp")
+let enusection = document.getElementById("ENUp")
+
+function BAEE(){
+  baesection.style.display = "flex"
+  enusection.style.display = "none"
+}
+function ENUU(){
+  baesection.style.display = "none"
+  enusection.style.display = "flex"
+}
+
+
+
+
+
+
