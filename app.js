6853256e0cb8462bdb1b7a5c6a607046e0bc8c30
waxedwindow <nayeemsultan2006@gmail.com>
2024-10-02 13:02:36 +0100

    update

---
+++ app.js
@@ -0,0 +1,19 @@
+
+let baesection = document.getElementById("BAEp")
+let enusection = document.getElementById("ENUp")
+
+function BAEE(){
+  baesection.style.display = "flex"
+  enusection.style.display = "none"
+}
+function ENUU(){
+  baesection.style.display = "none"
+  enusection.style.display = "flex"
+}
+
+
+
+
+
+
+
