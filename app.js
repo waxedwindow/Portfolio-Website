625d8109e7772fd68cc95fe058ddb61762fb4e4b
waxedwindow <nayeemsultan2006@gmail.com>
2024-10-08 15:33:33 +0100

    update i think?

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 
 let baesection = document.getElementById("BAEp")
 let enusection = document.getElementById("ENUp")
+let arduinosection = document.getElementById("Arduinop")
+let emulatesection = document.getElementById("Emulatep")
+
+//Experience Page
 
 function BAEE(){
   baesection.style.display = "flex"
@@ -9,6 +13,17 @@ function BAEE(){
 function ENUU(){
   baesection.style.display = "none"
   enusection.style.display = "flex"
+}
+
+// Project Page
+
+function Arduino(){
+  arduinosection.style.display = "flex"
+  emulatesection.style.display = "none"
+}
+function Emulate(){
+  arduinosection.style.display = "none"
+  emulatesection.style.display = "flex"
 }
 
 
