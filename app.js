9f52f9c8e1ba98b14a73d3c4ed461959b3f7ca9c
waxedwindow <nayeemsultan2006@gmail.com>
2024-10-10 00:28:01 +0100

    update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
-let baesection = document.getElementById("BAEp")
-let enusection = document.getElementById("ENUp")
-let arduinosection = document.getElementById("Arduinop")
-let emulatesection = document.getElementById("Emulatep")
 
 //Experience Page
+let baesection = document.getElementById("BAEp")
+let enusection = document.getElementById("ENUp")
+
+let cadetsubbutton = document.getElementById("cadet")
+let displayc = 0
 
 function BAEE(){
   baesection.style.display = "flex"
@@ -14,17 +15,67 @@ function ENUU(){
   baesection.style.display = "none"
   enusection.style.display = "flex"
 }
+function Cadet(){
+  if (displayc == 1)
+  {cadetsubbutton.style.display = "flex"
+    displayc = 0
+  }
+  else{
+    cadetsubbutton.style.display =  "none"
+    displayc = 1
+  }
+
+}
+
 
 // Project Page
+let websitesection = document.getElementById("Websitep")
 
+// Arduino Section 
+let arduinosubbutton = document.getElementById("arduinos")
+let arduinostartsection  = document.getElementById("ArduinoStart")
+let sevensegmentsection = document.getElementById("sevensegmentp")
+let displaya = 0
+// End of Arduino Section 
+let emulatesection = document.getElementById("emulationp")
+
+
+function Website(){
+  websitesection.style.display = "flex"
+  arduinostartsection.style.display = "none"
+  sevensegmentsection.style.display = "none"
+  emulatesection.style.display = "none"
+}
 function Arduino(){
-  arduinosection.style.display = "flex"
+  if (displaya == 1)
+    {arduinosubbutton.style.display = "flex"
+      displaya = 0
+    }
+    else{
+      arduinosubbutton.style.display =  "none"
+      displaya = 1
+    }
+}
+function ArduinoBeginning(){
+  websitesection.style.display = "none"
+  arduinostartsection.style.display = "flex"
+  sevensegmentsection.style.display = "none"
+  emulatesection.style.display = "none"
+}
+function SevenSegment(){
+  websitesection.style.display = "none"
+  arduinostartsection.style.display = "none"
+  sevensegmentsection.style.display = "flex"
   emulatesection.style.display = "none"
 }
 function Emulate(){
-  arduinosection.style.display = "none"
+  websitesection.style.display = "none"
+  arduinostartsection.style.display = "none"
+  sevensegmentsection.style.display = "none"
   emulatesection.style.display = "flex"
 }
+
+
 
 
 
