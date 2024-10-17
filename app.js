94f3b38fb915bcd3655460db1b4852c409b0edcb
waxedwindow <nayeemsultan2006@gmail.com>
2024-10-17 21:36:38 +0100

    update 17/10/2024

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let baesection = document.getElementById("BAEp")
 let enusection = document.getElementById("ENUp")
 
 let cadetsubbutton = document.getElementById("cadet")
-let displayc = 0
+let displayc = 1
 
 function BAEE(){
   baesection.style.display = "flex"
@@ -35,8 +35,16 @@ let websitesection = document.getElementById("Websitep")
 let arduinosubbutton = document.getElementById("arduinos")
 let arduinostartsection  = document.getElementById("ArduinoStart")
 let sevensegmentsection = document.getElementById("sevensegmentp")
-let displaya = 0
+
+let fourdigitsevensegmentsection = document.getElementById("fourdigitsevensegmentp")
+let foursevencodesection = document.getElementById("foursevencodep")
+
+let fakebombsection = document.getElementById("fakebombp")
+let fakebombcodesection = document.getElementById("fakebombcodep")
+
+let displaya = 1
 // End of Arduino Section 
+
 let emulatesection = document.getElementById("emulationp")
 
 
@@ -44,8 +52,15 @@ function Website(){
   websitesection.style.display = "flex"
   arduinostartsection.style.display = "none"
   sevensegmentsection.style.display = "none"
+  fourdigitsevensegmentsection.style.display = "none"
+  foursevencodesection.style.display = "none"
+  fakebombsection.style.display = "none"
+  fakebombcodesection.style.display = "none"
   emulatesection.style.display = "none"
 }
+
+// Arduino 
+
 function Arduino(){
   if (displaya == 1)
     {arduinosubbutton.style.display = "flex"
@@ -60,18 +75,71 @@ function ArduinoBeginning(){
   websitesection.style.display = "none"
   arduinostartsection.style.display = "flex"
   sevensegmentsection.style.display = "none"
+  fourdigitsevensegmentsection.style.display = "none"
+  foursevencodesection.style.display = "none"
+  fakebombsection.style.display = "none"
+  fakebombcodesection.style.display = "none"
   emulatesection.style.display = "none"
 }
 function SevenSegment(){
   websitesection.style.display = "none"
   arduinostartsection.style.display = "none"
   sevensegmentsection.style.display = "flex"
+  fourdigitsevensegmentsection.style.display = "none"
+  foursevencodesection.style.display = "none"
+  fakebombsection.style.display = "none"
+  fakebombcodesection.style.display = "none"
   emulatesection.style.display = "none"
 }
+function FourSevenSegment(){
+  websitesection.style.display = "none"
+  arduinostartsection.style.display = "none"
+  sevensegmentsection.style.display = "none"
+  fourdigitsevensegmentsection.style.display = "flex"
+  foursevencodesection.style.display = "none"
+  fakebombsection.style.display = "none"
+  fakebombcodesection.style.display = "none"
+  emulatesection.style.display = "none"
+}
+function FourSevenCode(){
+  websitesection.style.display = "none"
+  arduinostartsection.style.display = "none"
+  sevensegmentsection.style.display = "none"
+  fourdigitsevensegmentsection.style.display = "none"
+  foursevencodesection.style.display = "flex"
+  fakebombsection.style.display = "none"
+  fakebombcodesection.style.display = "none"
+  emulatesection.style.display = "none"
+}
+function FakeBomb(){
+  websitesection.style.display = "none"
+  arduinostartsection.style.display = "none"
+  sevensegmentsection.style.display = "none"
+  fourdigitsevensegmentsection.style.display = "none"
+  foursevencodesection.style.display = "none"
+  fakebombsection.style.display = "flex"
+  fakebombcodesection.style.display = "none"
+  emulatesection.style.display = "none"
+}
+function FakeBombCode(){
+  websitesection.style.display = "none"
+  arduinostartsection.style.display = "none"
+  sevensegmentsection.style.display = "none"
+  fourdigitsevensegmentsection.style.display = "none"
+  foursevencodesection.style.display = "none"
+  fakebombsection.style.display = "none"
+  fakebombcodesection.style.display = "flex"
+  emulatesection.style.display = "none" 
+}
+// End of Arduino 
 function Emulate(){
   websitesection.style.display = "none"
   arduinostartsection.style.display = "none"
   sevensegmentsection.style.display = "none"
+  fourdigitsevensegmentsection.style.display = "none"
+  foursevencodesection.style.display = "none"
+  fakebombsection.style.display = "none"
+  fakebombcodesection.style.display = "none"
   emulatesection.style.display = "flex"
 }
 
